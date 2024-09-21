a51ec5b5debe493ffc6a55009597fc84d1e90a8f
aamirali78654 <aamiralisargani786@gmail.com>
2024-09-21 22:05:19 +0500

    input editText id declair

--- app/src/main/java/com/example/demopractice/MainActivity.java
+++ app/src/main/java/com/example/demopractice/MainActivity.java
@@ -1,17 +1,23 @@
 package com.example.demopractice;
 
 import android.os.Bundle;
+import android.widget.EditText;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity
 {
+    EditText editText , editText1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editTextText);
+        editText1 = findViewById(R.id.password);
 
     }
 }
