7d0f4c1cadc8863248b7c1147f705becf8a43403
aamirali78654 <aamiralisargani786@gmail.com>
2024-09-21 22:07:14 +0500

    id's include editText

--- app/src/main/java/com/example/demopractice/MainActivity.java
+++ app/src/main/java/com/example/demopractice/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-
+        //-----------id's include editText--------------
         editText = findViewById(R.id.editTextText);
         editText1 = findViewById(R.id.password);
 
