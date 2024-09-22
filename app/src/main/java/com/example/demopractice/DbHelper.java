613f8cec17349067c60749fa595ab009f53e050b
aamirali78654 <aamiralisargani786@gmail.com>
2024-09-23 00:13:31 +0500

    DataBase create table

---
+++ app/src/main/java/com/example/demopractice/DbHelper.java
@@ -0,0 +1,56 @@
+package com.example.demopractice;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DbHelper extends SQLiteOpenHelper
+  {
+      private static final String DATABASE_NAME = "demo_db";
+      private static final int DATABASE_VERSION = 1;
+      //---------DbHelper's Constructor--------------->>
+      public DbHelper(@Nullable Context context)
+      {
+          super(context, DATABASE_NAME, null, DATABASE_VERSION);
+      }
+
+      @Override
+      public void onCreate(SQLiteDatabase db)
+      {
+          String CREATE_TABLE_QUERY = "CREATE TABLE register(id INTERGER PRIMARY KEY AUTOINCREMENT ,name TEXT, email TEXT, password TEXT, gender TEXT)";
+          db.execSQL(CREATE_TABLE_QUERY);
+
+      }
+
+      @Override
+      public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
+      {
+          db.execSQL("DROP TABLE IF EXISTS register");
+          onCreate(db);
+
+      }
+      public Boolean registerUserHepler(String name1, String email1, String pass1 , String gender1)
+      {
+
+          SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+          ContentValues contentValues = new ContentValues();
+
+          contentValues.put("name",name1);
+          contentValues.put("email",email1);
+          contentValues.put("password",pass1);
+          contentValues.put("gender",gender1);
+        Long l =   sqLiteDatabase.insert("register",null,contentValues);
+          sqLiteDatabase.close();
+          if(l>0)
+          {
+              return true;
+          }
+          else {
+              return false;
+          }
+
+      }
+  }
--- app/src/main/java/com/example/demopractice/MainActivity.java
+++ app/src/main/java/com/example/demopractice/MainActivity.java
@@ -1,16 +1,49 @@
 package com.example.demopractice;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity
 {
+    DbHelper dbHelper = new DbHelper(getApplicationContext());
+
+    EditText reg_name,reg_email, reg_pass, reg_gender;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        reg_email = (EditText) findViewById(R.id.reg_email);
+        reg_pass = (EditText) findViewById(R.id.reg_pass);
+        reg_gender = (EditText) findViewById(R.id.reg_gender);
+        reg_name = (EditText) findViewById(R.id.reg_name);
+    }
+
+    public void registerUser(View view)
+    {
+        String name = reg_name.getText().toString();
+        String email = reg_email.getText().toString();
+        String pass = reg_pass.getText().toString();
+        String gender = reg_gender.getText().toString();
+        //------DbHelper class method----------------------->>
+       Boolean b = dbHelper.registerUserHepler( name, email, pass, gender );
+       if(b == true)
+       {
+           Toast.makeText(this, "Register insert successfully...!!", Toast.LENGTH_SHORT).show();
+           reg_name.setText("");
+           reg_email.setText("");
+           reg_pass.setText("");
+           reg_gender.setText("");
+       }
+       else {
+           Toast.makeText(this, "Error...!", Toast.LENGTH_SHORT).show();
+       }
     }
 }
