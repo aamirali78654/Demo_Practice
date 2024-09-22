7d7d4250710580e3e833ed316925c61d555a5e90
aamirali78654 <aamiralisargani786@gmail.com>
2024-09-22 22:18:29 +0500

    demo practice java file

--- app/src/main/java/com/example/demopractice/MainActivity.java
+++ app/src/main/java/com/example/demopractice/MainActivity.java
@@ -1,23 +1,16 @@
 package com.example.demopractice;
 
 import android.os.Bundle;
-import android.widget.EditText;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity
 {
-    EditText editText , editText1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        //-----------id's include editText--------------
-        editText = findViewById(R.id.editTextText);
-        editText1 = findViewById(R.id.password);
-
     }
 }
