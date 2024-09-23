f147f4a0424aac3a86dbeb021b9cfc8a9258eece
aamirali78654 <aamiralisargani786@gmail.com>
2024-09-23 14:58:23 +0500

    database java file

--- app/src/main/java/com/example/demopractice/DbHelper.java
+++ app/src/main/java/com/example/demopractice/DbHelper.java
@@ -11,7 +11,7 @@ public class DbHelper extends SQLiteOpenHelper
   {
       private static final String DATABASE_NAME = "demo_db";
       private static final int DATABASE_VERSION = 1;
-      //---------DbHelper's Constructor--------------->>
+      //---------DbHelper's Constructor----------->>
       public DbHelper(@Nullable Context context)
       {
           super(context, DATABASE_NAME, null, DATABASE_VERSION);
